b423f7a17bc66a07dca3b562037c2d2d723bba8e
TheMissingText <dowlingscott016@gmail.com>
2020-12-18 19:16:31 +0000

    Added initial variables Added beenHit method Added setOccupied method

--- src/Locations.java
+++ src/Locations.java
@@ -1,2 +1,13 @@
 public class Locations {
+
+    private boolean hit = false;
+    private Object occupied = null;
+
+    public boolean beenHit(){
+        return this.hit;
+    }
+
+    public void setOccupied(Object occupation){
+        this.occupied = occupation;
+      }
 }
